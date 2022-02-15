1b2327cb8d649208aa446245013bbcb8f555f571
Reificios <95495067+Reificios@users.noreply.github.com>
2022-02-15 23:08:12 +0700

    Add GET ALL function

--- main.py
+++ main.py
@@ -25,3 +25,37 @@ db = client["watering"]
 robot_collection = db["robot_status"]
 tree_collection = db["default_tree"]
 record_collection = db["data_list"]
+
+@app.get("/getall/")
+def returnall():
+    all_tree = record_collection.find()
+    all = []
+    for tree in all_tree:
+        id = tree["tree_id"]
+        tree_info = tree_collection.find_one({"tree_id":id})
+
+        light = tree["light"][41]
+        humidity = tree["humidity"][41]
+        temp = tree["temp"][41]
+        
+        tmp = {
+            "tree_id" : id,
+            "base_light" : {
+                "set" : tree_info["base_light"],
+                "curret" : light
+                },
+            "base_humidity" : {
+                "set" : tree_info["base_humidity"],
+                "current" : humidity
+                },
+            "base_temp" : {
+                "set" : tree_info["base_temp"],
+                "current" : temp
+                },
+        }
+        all.append(tmp)
+    return {
+        "res_amount" : len(all),
+        "result" : all
+    }
+        
