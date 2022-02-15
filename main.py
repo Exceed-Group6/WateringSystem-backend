cbcc559232a8a796a63aed0f4548fffd9a212aaa
Reificios <95495067+Reificios@users.noreply.github.com>
2022-02-15 22:44:20 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from unittest import result
+from pymongo import MongoClient
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = MongoClient('mongodb://localhost', 27017)
+db = client["watering"]
+robot_collection = db["robot_status"]
+tree_collection = db["default_tree"]
+record_collection = db["data_list"]
