13d951ef850e42741ab8c303b07b6f0f45671f43
Reificios <95495067+Reificios@users.noreply.github.com>
2022-02-16 10:15:19 +0700

    delete tree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from unittest import result
 from pymongo import MongoClient
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from typing import Optional
 from fastapi.encoders import jsonable_encoder
@@ -25,3 +25,18 @@ db = client["watering"]
 robot_collection = db["robot_status"]
 tree_collection = db["default_tree"]
 record_collection = db["data_list"]
+
+@app.delete("/deletetree/{tree_id}/")
+def delete_tree(tree_id : int):
+    query = {"tree_id" : tree_id}
+    test = robot_collection.find_one(query)
+    if test == None:
+        return{
+            "result" : "not found"
+        }
+    robot_collection.delete_one(query)
+    tree_collection.delete_one(query)
+    record_collection.delete_one(query)
+    return{
+        "result" : "success"
+    }
