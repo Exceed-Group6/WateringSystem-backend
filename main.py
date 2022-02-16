dd576f604a6dc89b2fb94f4b606a94d122b5b2cf
Reificios <95495067+Reificios@users.noreply.github.com>
2022-02-16 08:55:51 +0700

    add get by id

--- main.py
+++ main.py
@@ -25,3 +25,30 @@ db = client["watering"]
 robot_collection = db["robot_status"]
 tree_collection = db["default_tree"]
 record_collection = db["data_list"]
+
+@app.get("/getbyid/{tree_id}")
+def returnbyid(tree_id : int):
+    robot = robot_collection.find_one({"tree_id" : tree_id})
+    find = tree_collection.find_one({"tree_id" : tree_id})
+    tree = record_collection.find_one({"tree_id":tree_id})
+    light = tree["light"][41]
+    humidity = tree["humidity"][41]
+    temp = tree["temp"][41]
+    return{
+        "tree_name" : find["name"],
+        "tree_desc" : find["desc"],
+        "cur_bot_status": robot["mode_status"],
+        "cur_bot_duration" : robot["duration"],
+        "base_light" : {
+                "set" : find["base_light"],
+                "curret" : light
+                },
+        "base_humidity" : {
+            "set" : find["base_humidity"],
+            "current" : humidity
+            },
+        "base_temp" : {
+            "set" : find["base_temp"],
+            "current" : temp
+            },
+    }
