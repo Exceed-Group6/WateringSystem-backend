218f7ba7596aa3492ef9a03e9c59dd9aa50c550b
Reificios <95495067+Reificios@users.noreply.github.com>
2022-02-19 10:47:30 +0700

    UPDATE POST AND DELETE

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydoc import doc
 from unittest import result
 from pymongo import MongoClient
 from fastapi import FastAPI, HTTPException, Query
@@ -45,6 +46,15 @@ class default_tree(BaseModel):
     base_humidity: list
     base_temp: list
 
+class input_tree(BaseModel):
+    name: str
+    desc: str
+    base_light: list
+    base_humidity: list
+    base_temp: list
+    mode_status :  int
+    duration : int
+
 @app.put("/updatecommand")
 def update_robot_status(ro_status : robot_status):
     r = jsonable_encoder(ro_status)
@@ -54,10 +64,34 @@ def update_robot_status(ro_status : robot_status):
     return {
         "result" : "update success"
     }
+
 @app.post("/postnewtree")
-def postnewtree(def_tree: default_tree):
+def postnewtree(def_tree: input_tree):
+    count = tree_collection.count_documents({}) + 1
     t = jsonable_encoder(def_tree)
-    tree_collection.insert_one(t)
+    tree = {
+        "tree_id" : count,
+        "name" : t["name"],
+        "desc" : t["desc"],
+        "base_light" : t["base_light"],
+        "base_humidity" : t["base_humidity"],
+        "base_temp" : t["base_temp"]
+    }
+    robot = {
+        "tree_id" : count,
+        "mode_status" : t["mode_status"],
+        "duration" : t["duration"]
+    }
+    record = {
+        "tree_id" : count,
+        "light" : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        "humidity" : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        "temp" : [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    }
+    tree_collection.insert_one(tree)
+    robot_collection.insert_one(robot)
+    record_collection.insert_one(record)
+
     return {
         "result" : "success"
     }
@@ -69,16 +103,20 @@ def returnall():
     for tree in all_tree:
         id = tree["tree_id"]
         tree_info = tree_collection.find_one({"tree_id":id})
+        robot_info = robot_collection.find_one({"tree_id":id})
 
         light = tree["light"][41]
         humidity = tree["humidity"][41]
         temp = tree["temp"][41]
         
         tmp = {
-            "tree_id" : id,
+            "id" : id,
+            "name" : tree_info["name"],
+            "mode_status" : robot_info["mode_status"],
+            "duration" : robot_info["duration"],
             "base_light" : {
                 "set" : tree_info["base_light"],
-                "curret" : light
+                "current" : light
                 },
             "base_humidity" : {
                 "set" : tree_info["base_humidity"],
@@ -110,7 +148,7 @@ def returnbyid(tree_id : int):
         "cur_bot_duration" : robot["duration"],
         "base_light" : {
                 "set" : find["base_light"],
-                "curret" : light
+                "current" : light
                 },
         "base_humidity" : {
             "set" : find["base_humidity"],
@@ -143,12 +181,29 @@ def delete_tree(tree_id : int):
         return{
             "result" : "not found"
         }
-    robot_collection.delete_one(query)
-    tree_collection.delete_one(query)
-    record_collection.delete_one(query)
-    return{
-        "result" : "success"
-    }
+    else:
+        robot_collection.delete_one(query)
+        tree_collection.delete_one(query)
+        record_collection.delete_one(query)
+
+        update1 = robot_collection.find({"tree_id" : {"$gt":tree_id}})
+        update2 = tree_collection.find({"tree_id" : {"$gt":tree_id}})
+        update3 = record_collection.find({"tree_id" : {"$gt":tree_id}})
+
+        # robot_collection.update_many({"tree_id" : {"$gt":tree_id}}, {"tree_id" : {"$toInt":"$tree_id"}-1})
+        # tree_collection.update_many({"tree_id" : {"$gt":tree_id}}, {"tree_id" : {"$toInt":"$tree_id"}-1})
+        # record_collection.update_many({"tree_id" : {"$gt":tree_id}}, {"tree_id" : {"$toInt":"$tree_id"}-1})
+
+        for document in update1:
+            cur_id = document["tree_id"]
+            set_id = document["tree_id"] -1
+            robot_collection.update_one({"tree_id" : cur_id},{"$set" : { "tree_id" : set_id}})
+            tree_collection.update_one({"tree_id" : cur_id},{"$set" : { "tree_id" : set_id}})
+            record_collection.update_one({"tree_id" : cur_id},{"$set" : { "tree_id" : set_id}})
+
+        return{
+            "result" : "success"
+        }
 
 @app.get("/command/{tree_id}")
 def returnrobotcommand(tree_id : int):
